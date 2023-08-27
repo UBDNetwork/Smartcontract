531210084abcf19600a1346be983f2185f6c6ae6
Alex <alexamuek@gmail.com>
2023-08-27 19:23:11 +0800

    update test_exchange_a_05.py

--- tests/test_exchange_a_05.py
+++ tests/test_exchange_a_05.py
@@ -3,12 +3,12 @@ import logging
 from brownie import Wei, reverts, chain
 LOGGER = logging.getLogger(__name__)
 
-PAY_AMOUNT = 1000e6
-MINT_UBD_AMOUNT = 1000e18
+USDT_AMOUNT = 1000e6
+UBD_AMOUNT = 1000e18
 ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 def test_usdt_to_ubd(accounts, ubd_exch, exchange_single, usdt, dai):
-    global PAY_AMOUNT
+    global USDT_AMOUNT
     #prepare data
     
     #check calcOutUBDForExactInBASE
@@ -17,16 +17,35 @@ def test_usdt_to_ubd(accounts, ubd_exch, exchange_single, usdt, dai):
     exchange_single.setUBDToken(ubd_exch, {'from':accounts[0]})
     exchange_single.setBeneficiary(accounts[1], {'from':accounts[0]})
 
-    outAmount = exchange_single.calcOutUBDForExactInBASE(PAY_AMOUNT)
+    outAmount_ubd = exchange_single.calcOutUBDForExactInBASE(USDT_AMOUNT)
 
-    fee = round(PAY_AMOUNT*fee_percent/(100+fee_percent))
-    calcOutAmount = (PAY_AMOUNT-fee)*10**ubd_exch.decimals()/10**usdt.decimals()
+    fee = round(USDT_AMOUNT*fee_percent/(100+fee_percent))
+    calcOutAmount_ubd = (USDT_AMOUNT-fee)*10**ubd_exch.decimals()/10**usdt.decimals()
 
-    assert outAmount == calcOutAmount
+    assert outAmount_ubd == calcOutAmount_ubd
 
     #check calcInBASEForExactOutUBD
-    calcInAmount = exchange_single.calcInBASEForExactOutUBD(outAmount)
-    assert PAY_AMOUNT == calcOutAmount
+
+    #!!!!REVERT
+    
+    calcInAmount_usdt = exchange_single.calcInBASEForExactOutUBD(outAmount_ubd)
+    assert USDT_AMOUNT == calcOutAmount_usdt
+
+    #check calcOutBASEForExactInUBD
+    fee_percent = exchange_single.paymentTokens(ubd_exch.address)[1]/exchange_single.PERCENT_DENOMINATOR()
+    outAmount_usdt = exchange_single.calcOutBASEForExactInUBD(UBD_AMOUNT)
+
+    fee = round(UBD_AMOUNT*fee_percent/(100+fee_percent))
+    calcOutAmount_usdt = (UBD_AMOUNT-fee)*10**usdt.decimals()/10**ubd_exch.decimals()
+
+    assert outAmount_usdt == calcOutAmount_usdt
+
+    #check calcInUBDForExactOutBASE
+    calcInAmount_ubd = exchange_single.calcInUBDForExactOutBASE(outAmount_usdt)
+
+    assert UBD_AMOUNT - calcInAmount_ubd <= 10**13
+
+
 
 
 
