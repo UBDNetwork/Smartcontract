45e865932df5b419e41839d5a6d2af2af4c6dc54
Alex <alexamuek@gmail.com>
2023-09-29 11:58:53 +0800

    add test_sandbox_a_07.py

---
+++ tests/test_sandbox_a_07.py
@@ -0,0 +1,96 @@
+import pytest
+import logging
+import math
+from brownie import Wei, reverts, chain
+from help_init_registry import init_market_registry
+LOGGER = logging.getLogger(__name__)
+
+PAY_AMOUNT = 1005e6
+MINT_UBD_AMOUNT = 1000e18
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+def test_usdt_to_ubd(accounts, ubd, sandbox1, usdt):
+    logging.info('!!!!!!!!!!!!!!!!!   exchange from usdt to ubd    !!!!!!!!!!!!!!!')
+    sandbox1.setUBDToken(ubd, {'from':accounts[0]})
+    sandbox1.setBeneficiary(accounts[1], {'from':accounts[0]})
+    
+    usdt.approve(sandbox1, PAY_AMOUNT, {'from':accounts[0]})
+    
+    chain.sleep(10)
+    
+    tx = sandbox1.swapExactInput(
+        usdt, 
+        PAY_AMOUNT,
+        0,
+        0,
+        ZERO_ADDRESS, 
+        {'from':accounts[0]}
+    )
+    assert tx.return_value == MINT_UBD_AMOUNT
+    assert ubd.balanceOf(accounts[0]) == MINT_UBD_AMOUNT
+
+
+
+def test_usdt_to_ubd_100k_1(accounts, ubd, sandbox1, usdt):
+    usdt.approve(sandbox1, PAY_AMOUNT * 100, {'from':accounts[0]})
+
+    before_ubd_acc0 = ubd.balanceOf(accounts[0])
+    
+    tx = sandbox1.swapExactInput(
+        usdt, 
+        PAY_AMOUNT * 100,
+        0,
+        0,
+        ZERO_ADDRESS, 
+        {'from':accounts[0]}
+    )
+    
+    assert tx.return_value == MINT_UBD_AMOUNT * 100
+    assert ubd.balanceOf(accounts[0]) == before_ubd_acc0 + MINT_UBD_AMOUNT * 100
+
+def test_topup_treasury_from_sandbox1(
+        accounts, mockuniv2, dai, usdt, sandbox1, sandbox2, 
+        treasury, ubd, markets, wbtc, market_adapter, weth, usdc):
+    logging.info('!!!!!!!!!!!!!!!!!   topup treasury from sandbox1 - there is slippage in defi 50%, there is slippage in marketRegistry 1%  !!!!!!!!!!!!!!!')
+    mockuniv2.setSlippgae(50)
+    init_market_registry(accounts, mockuniv2, dai, usdt, sandbox1, sandbox2, treasury, ubd, markets, wbtc, market_adapter, weth, usdc)
+    logging.info(
+        '\nSandbox1.balance(usdt):{}'
+        '\ntreasury.balance(wbtc):{}'
+        '\ntreasury.balance(eth):{}'.format(
+            usdt.balanceOf(sandbox1),
+            wbtc.balanceOf(treasury),
+            treasury.balance()
+    ))
+    logging.info('UBDNetwork.state:{}'.format(markets.ubdNetwork()))
+    logging.info('UBDNetwork.state:{}'.format(markets.getUBDNetworkInfo()))
+    accounts[2].transfer(mockuniv2, accounts[2].balance()-1e18)
+
+    #sandbox has 1% of balance > 1000 usdt - can exchange usdt to ether and wbtc
+    before_usdt_sandbox = usdt.balanceOf(sandbox1.address)
+    logging.info(markets.markets(usdt))
+    with reverts('UniSwapV2: Excuse me, slippage. Try again with other settings'):
+        tx = sandbox1.topupTreasury({'from':accounts[1]})
+
+    #change settings
+    mockuniv2.setSlippgae(1) # valid slippage persent . MarketRegistry has 1% slippage for assets
+    tx = sandbox1.topupTreasury({'from':accounts[1]})
+
+
+    assert usdt.balanceOf(sandbox1.address) == before_usdt_sandbox - before_usdt_sandbox/100  #-1%
+
+
+    logging.info('Ether transfer:{}'.format(tx.events['ReceivedEther']))
+    [logging.info('\nfrom:{} to:{} value:{}'.format(x['from'],x['to'],x['value'])) for x in tx.events['Transfer']]
+    logging.info(
+        '\nSandbox1.balance(usdt):{}'
+        '\ntreasury.balance(wbtc):{}'
+        '\ntreasury.balance(eth):{}'.format(
+            usdt.balanceOf(sandbox1),
+            wbtc.balanceOf(treasury),
+            treasury.balance()
+    ))
+
+    #exchange 1% usdt of sandbox1 to swap => 50% ETH and 50% WBTC + 1% slippage in defi happened - treasury got assets 1% less due to slippage
+    assert wbtc.balanceOf(treasury) - (before_usdt_sandbox*99/100/100/2/mockuniv2.rates(usdt.address, wbtc.address)[0])*10**wbtc.decimals()/10**usdt.decimals() <=100
+    assert (before_usdt_sandbox*99/100/100/2/mockuniv2.rates(usdt.address, weth.address)[0])*10**weth.decimals()/10**usdt.decimals() - treasury.balance()  <= 3000000000000
+
