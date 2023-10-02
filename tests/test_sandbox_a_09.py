6116d31bfed0f1b08aceff4ee790c32684bbedae
Alex <alexamuek@gmail.com>
2023-10-02 18:47:35 +0800

    add test_sandbox_a_09.py

---
+++ tests/test_sandbox_a_09.py
@@ -0,0 +1,76 @@
+import pytest
+import logging
+import math
+from brownie import Wei, reverts, chain
+from help_init_registry import init_market_registry
+LOGGER = logging.getLogger(__name__)
+
+PAY_AMOUNT = 1005e6
+MINT_UBD_AMOUNT = 1000e18
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+def test_usdt_to_ubd(accounts, ubd, sandbox1, usdt):
+    logging.info('!!!!!!!!!!!!!!!!!   exchange from usdt to ubd    !!!!!!!!!!!!!!!')
+    sandbox1.setUBDToken(ubd, {'from':accounts[0]})
+    sandbox1.setBeneficiary(accounts[1], {'from':accounts[0]})
+    
+    usdt.approve(sandbox1, PAY_AMOUNT, {'from':accounts[0]})
+    
+    chain.sleep(10)
+    
+    tx = sandbox1.swapExactInput(
+        usdt, 
+        PAY_AMOUNT,
+        0,
+        0,
+        ZERO_ADDRESS, 
+        {'from':accounts[0]}
+    )
+    assert tx.return_value == MINT_UBD_AMOUNT
+    assert ubd.balanceOf(accounts[0]) == MINT_UBD_AMOUNT
+
+
+
+def test_usdt_to_ubd_100k_1(accounts, ubd, sandbox1, usdt):
+    usdt.approve(sandbox1, PAY_AMOUNT * 100, {'from':accounts[0]})
+
+    before_ubd_acc0 = ubd.balanceOf(accounts[0])
+    
+    tx = sandbox1.swapExactInput(
+        usdt, 
+        PAY_AMOUNT * 100,
+        0,
+        0,
+        ZERO_ADDRESS, 
+        {'from':accounts[0]}
+    )
+    
+    assert tx.return_value == MINT_UBD_AMOUNT * 100
+    assert ubd.balanceOf(accounts[0]) == before_ubd_acc0 + MINT_UBD_AMOUNT * 100
+
+def test_topup_change_BASE_ASSET_in_sandbox1(
+        accounts, mockuniv2, dai, usdt, sandbox1, sandbox2, 
+        treasury, ubd, markets, wbtc, market_adapter, weth, usdc):
+    logging.info('!!!!!!!!!!!!!!!!!   change BASE_ASSET of sandbox1 when there was not topup of treasury. Try to exchange ubd  !!!!!!!!!!!!!!!')
+    init_market_registry(accounts, mockuniv2, dai, usdt, sandbox1, sandbox2, treasury, ubd, markets, wbtc, market_adapter, weth, usdc)
+    logging.info(
+        '\nSandbox1.balance(usdt):{}'
+        '\ntreasury.balance(wbtc):{}'
+        '\ntreasury.balance(eth):{}'.format(
+            usdt.balanceOf(sandbox1),
+            wbtc.balanceOf(treasury),
+            treasury.balance()
+    ))
+
+    logging.info('getCollateralLevelM10 = {}'.format(markets.getCollateralLevelM10()))
+    sandbox1.setPaymentTokenStatus(dai, True, 5000, {"from": accounts[0]})
+    logging.info('getCollateralLevelM10 = {}'.format(markets.getCollateralLevelM10()))
+
+    ubd.approve(sandbox1, 100000*10**ubd.decimals(), {"from": accounts[0]})
+    tx = sandbox1.swapExactInput(ubd.address, 
+                                1*10**ubd.decimals(),
+                                0,
+                                1*10**usdt.decimals())
+    assert tx.return_value == 0
+    logging.info(tx.events)
+    
+    
